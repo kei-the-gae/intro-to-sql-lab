608581b57928d86e92c48f213945d09a32e3a1d3
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-09-09 13:48:18 -0700

    Solve clue 4

--- clues.sql
+++ clues.sql
@@ -18,7 +18,7 @@ SELECT code, name, region FROM countries WHERE code = 'SMR';
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
 -- Write SQL query here
-
+SELECT * FROM cities WHERE countrycode = 'SMR' AND NOT name = 'San Marino';
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
