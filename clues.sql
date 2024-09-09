7b6deedd346e129935f3415ebc595a1c79f276a2
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-09-09 14:18:11 -0700

    Solve clue 5

--- clues.sql
+++ clues.sql
@@ -23,7 +23,7 @@ SELECT * FROM cities WHERE countrycode = 'SMR' AND NOT name = 'San Marino';
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
-
+SELECT * FROM cities WHERE name LIKE 'San M_____' AND countrycode in ('ARG', 'BOL', 'BRA', 'CHL', 'COL', 'ECU', 'FLK', 'GUF', 'GUY', 'PER', 'PRY', 'SUR', 'URY', 'VEN');
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
