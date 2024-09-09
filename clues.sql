9bd0ba0d8f52aafe213601dd336371b242de8a81
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-09-09 14:22:13 -0700

    Solve clue 6

--- clues.sql
+++ clues.sql
@@ -30,7 +30,8 @@ SELECT * FROM cities WHERE name LIKE 'San M_____' AND countrycode in ('ARG', 'BO
 -- follow right behind you!
 
 -- Write SQL query here
-
+SELECT code, name, capital FROM countries WHERE code = 'ARG';
+SELECT * FROM cities WHERE id = '69';
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
 
