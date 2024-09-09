d3bc41798fda89b0613791407c9a92078e8a1a5b
Kei Holland <85800932+kei-the-gae@users.noreply.github.com>
2024-09-09 13:38:55 -0700

    Solve clue 3

--- clues.sql
+++ clues.sql
@@ -12,7 +12,8 @@ SELECT countrycode, language FROM countrylanguages WHERE countrycode = 'VAT' AND
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
 -- Write SQL query here
-
+SELECT * FROM countrylanguages WHERE language = 'Italian' AND percentage = 100;
+SELECT code, name, region FROM countries WHERE code = 'SMR';
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
